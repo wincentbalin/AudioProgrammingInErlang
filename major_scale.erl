5f6a6bb757dd55a4f51bb24e9c41e18fef1ddd87
Wincent Balin <wincent.balin@gmail.com>
2011-08-05 21:46:24 +0200

    Added another music theory example

---
+++ major_scale.erl
@@ -0,0 +1,39 @@
+-module(major_scale).
+-author('Wincent Balin').
+-export([main/0]).
+
+% Translate pitch class to note name
+note_name(0) -> "C";
+note_name(1) -> "Db";
+note_name(2) -> "D";
+note_name(3) -> "Eb";
+note_name(4) -> "E";
+note_name(5) -> "F";
+note_name(6) -> "Gb";
+note_name(7) -> "G";
+note_name(8) -> "Ab";
+note_name(9) -> "A";
+note_name(10) -> "Bb";
+note_name(11) -> "B".
+
+% Build scale
+build_scale(Key) ->
+	build_scale(Key, "", 0).
+
+build_scale(Key, Scale, Index) when Index < 7 ->
+	Pitch_step =
+		case Index of
+			2 -> 1;
+			_ -> 2
+		end,
+	Next_key = (Key + Pitch_step) rem 12,
+	Next_scale = lists:concat([Scale, note_name(Key), " "]),
+	build_scale(Next_key, Next_scale, Index + 1);
+build_scale(_, Scale, _) ->
+	Scale.
+
+main() ->
+	Note_input = io:get_line("Please enter the key (in pitch-class number, 0-11): "),
+	{Note, _} = string:to_integer(Note_input),
+	io:format("~s~n", [build_scale(Note)]).
+	
